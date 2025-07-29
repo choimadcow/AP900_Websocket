dc42956646ce5c28a4c34e9bde0935d60fea624e
choikw <ckw@sothis.co.kr>
2025-07-29 11:05:04 +0900

    delete ip limitation

--- app.ts
+++ app.ts
@@ -29,30 +29,7 @@ import {changeDriveMode, createMockData} from './services/mock-data';
 let timer: NodeJS.Timeout | null = null;
 let timer2: NodeJS.Timeout | null = null;
 
-const allowedIps = ['14.34.84.138', '::1', '127.0.0.1', '*']; // 허용할 실제 공인 IP를 여기에 추가하세요.
 
-const ipFilter = (req: Request, res: Response, next: NextFunction) => {
-    const forwardedFor = req.headers['x-forwarded-for'];
-    let clientIp: string | undefined;
-
-    if (typeof forwardedFor === 'string') {
-        // X-Forwarded-For 헤더가 있으면, 가장 첫 번째 IP를 실제 클라이언트 IP로 간주합니다.
-        clientIp = forwardedFor.split(',')[0].trim();
-    } else {
-        // 헤더가 없으면 req.ip를 사용합니다 (로컬 개발 환경 등).
-        clientIp = req.ip;
-    }
-
-    console.log(`Access attempt. Real Client IP: ${clientIp}, X-Forwarded-For: ${forwardedFor}, req.ip: ${req.ip}`);
-
-    // if (clientIp && allowedIps.includes(clientIp)) {
-        next();
-    // } else {
-    //     res.status(403).send('Forbidden: Access is denied');
-    // }
-};
-
-app.use(ipFilter);
 
 app.get("/auto-start", (req: Request, res: Response) => {
     console.log("자율주행 운행 시작!");
