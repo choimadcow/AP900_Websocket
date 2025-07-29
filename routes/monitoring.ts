73b05ccffb6899f24cf4a4c5062076b4bc553843
choikw <ckw@sothis.co.kr>
2025-07-29 20:24:35 +0900

    send error info

--- routes/monitoring.ts
+++ routes/monitoring.ts
@@ -3,6 +3,7 @@ import path from 'path';
 const router = express.Router();
 
 let passengerCount: number | null = null; // 승객 수를 저장할 변수
+let errInfo: Boolean | null = null;
 
 /* GET monitoring page. */
 router.get('/', function(req: Request, res: Response) {
@@ -24,6 +25,22 @@ router.get('/passenger-count/:param', (req: Request, res: Response) => {
 /* GET API for passenger count. */
 router.get('/api/passenger-count', (req: Request, res: Response) => {
   res.json({ passengerCount: passengerCount });
+});
+
+router.get('/error-info/:param', (req: Request, res: Response) => {
+  const errType: boolean = Boolean(req.params.param);
+  if (errType) {
+    errInfo = errType;
+    console.log(`Passenger count updated to: ${errType ? "객체인식오류" : "Take Over"}`);
+    res.status(200).send(`Passenger count updated to ${errType ? "객체인식오류" : "Take Over"}`);
+  } else {
+    res.status(400).send('Invalid passenger count parameter.');
+  }
+});
+
+/* GET API for passenger count. */
+router.get('/api/error-info', (req: Request, res: Response) => {
+  res.json({ errInfo: errInfo });
 });
 
 export default router;
